1844c9af12a5ea53bfa36095b4c024fb4498128e
Kutkin Oleg <kutkino@list.ru>
2023-04-06 11:54:21 +0700

    ninth

--- SKIF_1-5/pickle_reader.py
+++ SKIF_1-5/pickle_reader.py
@@ -7,20 +7,44 @@ import os
 
 
 
-tab=[]
+tabx=[]
+taby=[]
+tabz=[]
 x=[]
+y=[]
+z=[]
 E=30000
+for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-z"):
+    if file.endswith(".pickle"):
+        g=pickle.load(open(os.path.join(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-z", file), 'rb'))
+        temp, chan = file.split('_')
+        chan=chan.replace('.0.pickle', '')
+        z.append(int(chan))
+        tabz.append(g.flux)
+
+
+plt.plot(z, tabz, '.')
+plt.show()
+for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-x"):
+    if file.endswith(".pickle"):
+        g=pickle.load(open(os.path.join(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-x", file), 'rb'))
+        temp, chan = file.split('_')
+        chan=chan.replace('.0.pickle', '')
+        x.append(int(chan))
+        tabx.append(g.flux)
+
+
+plt.plot(x, tabx, '.')
+plt.show()
 for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-y"):
     if file.endswith(".pickle"):
         g=pickle.load(open(os.path.join(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-y", file), 'rb'))
         temp, chan = file.split('_')
         chan=chan.replace('.0.pickle', '')
-        x.append(int(chan))
-        tab.append(g.flux)
+        y.append(int(chan))
+        taby.append(g.flux)
 
 
-plt.plot(x, tab, '.')
-Rd = np.arange(-150., 150, 0.2)
-
+plt.plot(y, taby, '.')
 
 plt.show()
