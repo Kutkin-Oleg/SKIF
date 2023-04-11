9e17608e2af962d5f4222abd62dc5c676c9c8eb8
Kutkin Oleg <kutkino@list.ru>
2023-04-12 00:13:59 +0700

    110423-midnight

--- SKIF_1-5/pickle_reader.py
+++ SKIF_1-5/pickle_reader.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from scipy.optimize import curve_fit
-from numpy.polynomial.polynomial import Polynomial
-from scipy.interpolate import lagrange
+
 
 x=np.array([],dtype='f')
 y=np.array([],dtype='f')
@@ -28,6 +27,28 @@ def func_G(x, a, b, c, d):
 
 def get_G_FWHM(c):
  return 2*c*(2*np.log(2))**0.5
+
+def get_FWHM(x, y, maxy):
+    maxy=0.5*maxy
+    # print('FW = %s' % maxy)
+    if min(x)<0:
+        mid=(abs(max(x))-abs(min(x)))/2
+    else:
+        mid=(abs(max(x))+abs(min(x)))/2
+
+    FWHM=0.
+    for i in np.arange(round(len(x)/2), len(x), 1):
+        if (y[i] >= maxy*0.98) and (y[i] <= maxy*1.02):
+            FWHM=abs(x[i]-mid)
+            # print('max1 = %s' % y[i])
+            break
+
+    for i in np.arange(round(len(x)/2), 0, -1):
+        if (y[i] >= maxy * 0.98) and (y[i] <= maxy * 1.02):
+            FWHM=(FWHM+abs(x[i]-mid))
+            # print('max2 = %s' % y[i])
+            break
+    return FWHM
 
 def lagrange(x, y, t):
     """
@@ -69,7 +90,9 @@ for i in np.arange(0, len(line), 1):
     y_line =np.append(y_line, lagrange(x, tabx, line[i]))
 plt.ylim([0, 1.e13])
 plt.plot(line, y_line, 'g-')
-print('x_FWHM=%s'%get_FWHM( popt[2]))
+print('x_G_FWHM=%s' % get_G_FWHM(popt[2]))
+print('x_FWHM=%s' % get_FWHM(line, y_line, max(tabx)))
+
 for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-y"):
     if file.endswith(".pickle"):
         g=pickle.load(open(os.path.join(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-y", file), 'rb'))
@@ -83,7 +106,15 @@ plt.plot(y, taby, '.')
 popt, pcov = curve_fit(func_G, y, taby, p0=[max(taby), sum(y*taby)/sum(taby), np.std(y), 0] ) #среднеквадр
 y_line = np.arange(min(y), max(y), 1)
 plt.plot(y_line, func_G(y_line, *popt), 'r-')
-print('y_FWHM=%s'%get_FWHM( popt[2]))
+print('y_G_FWHM=%s' % get_G_FWHM(popt[2]))
+
+y_line =[]
+line =np.arange(min(y), max(y), 0.5)
+for i in np.arange(0, len(line), 1):
+    y_line =np.append(y_line, lagrange(y, taby, line[i]))
+plt.ylim([0, 1.e13])
+plt.plot(line, y_line, 'g-')
+print('y_FWHM=%s' % get_FWHM(line, y_line, max(taby)))
 
 for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-z"):
     if file.endswith(".pickle"):
@@ -98,7 +129,15 @@ plt.plot(z, tabz, '.')
 popt, pcov = curve_fit(func_G, z, tabz, p0=[max(tabz), sum(z*tabz)/sum(tabz), np.std(z), 0])
 z_line = np.arange(min(z), max(z), 1)
 plt.plot(z_line, func_G(z_line, *popt), 'r-')
-print('z_FWHM=%s'%get_FWHM( popt[2]))
+print('z_G_FWHM=%s' % get_G_FWHM(popt[2]))
+
+y_line=[]
+line =np.arange(min(z), max(z), 0.1)
+for i in np.arange(0, len(line), 1):
+    y_line =np.append(y_line, lagrange(z, tabz, line[i]))
+plt.ylim([0, 1.e13])
+plt.plot(line, y_line, 'g-')
+print('Z_FWHM=%s' % get_FWHM(line, y_line, max(tabz)))
 
 for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-pitch"):
     if file.endswith(".pickle"):
@@ -113,7 +152,15 @@ plt.plot(pitch, tabpitch, '.')
 popt, pcov = curve_fit(func_G, pitch, tabpitch,p0=[max(tabpitch), sum(pitch*tabpitch)/sum(tabpitch), np.std(pitch), 0])
 pitch_line = np.arange(min(pitch), max(pitch), 1.e-6)
 plt.plot(pitch_line, func_G(pitch_line, *popt), 'r-')
-print('pitch_FWHM=%s'%get_FWHM( popt[2]))
+print('pitch_G_FWHM=%s' % get_G_FWHM(popt[2]))
+
+y_line=[]
+line =np.arange(min(pitch), max(pitch), 1.e-6)
+for i in np.arange(0, len(line), 1):
+    y_line =np.append(y_line, lagrange(pitch, tabpitch, line[i]))
+plt.ylim([0, 1.e13])
+plt.plot(line, y_line, 'g-')
+print('pitch_FWHM=%s' % get_FWHM(line, y_line, max(tabpitch)))
 
 for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-roll"):
     if file.endswith(".pickle"):
@@ -128,7 +175,15 @@ plt.plot(roll, tabroll, '.')
 popt, pcov = curve_fit(func_G, roll, tabroll,p0=[max(tabroll), sum(roll*tabroll)/sum(tabroll), np.std(roll), 0])
 roll_line = np.arange(min(roll), max(roll), 1.e-3)
 plt.plot(roll_line, func_G(roll_line, *popt), 'r-')
-print('roll_FWHM=%s'%get_FWHM( popt[2]))
+print('roll_G_FWHM=%s' % get_G_FWHM(popt[2]))
+
+y_line=[]
+line =np.arange(min(roll), max(roll), 0.1e-3)
+for i in np.arange(0, len(line), 1):
+    y_line =np.append(y_line, lagrange(roll, tabroll, line[i]))
+plt.ylim([0, 1.e13])
+plt.plot(line, y_line, 'g-')
+print('roll_FWHM=%s' % get_FWHM(line, y_line, max(tabroll)))
 
 
 for file in os.listdir(r"C:\Users\synchrotron\PycharmProjects\SKIF\change-yaw"):
@@ -144,6 +199,14 @@ plt.plot(yaw, tabyaw, '.')
 popt, pcov = curve_fit(func_G, yaw, tabyaw, p0=[max(tabyaw), sum(yaw*tabyaw)/sum(tabyaw), np.std(yaw), 0])
 yaw_line = np.arange(min(yaw), max(yaw), 1.e-3, )
 plt.plot(yaw_line, func_G(yaw_line, *popt), 'r-')
-print('yaw_FWHM=%s'%get_FWHM( popt[2]))
+print('yaw_G_FWHM=%s' % get_G_FWHM(popt[2]))
+
+y_line=[]
+line =np.arange(min(yaw), max(yaw), 1.e-3)
+for i in np.arange(0, len(line), 1):
+    y_line =np.append(y_line, lagrange(yaw, tabyaw, line[i]))
+plt.ylim([0, 1.e13])
+plt.plot(line, y_line, 'g-')
+print('yaw_FWHM=%s' % get_FWHM(line, y_line, max(tabyaw)))
 
 plt.show()
