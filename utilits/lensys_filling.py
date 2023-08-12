5fbca948a05ae1e52053532720ad1794661fe63a
Kutkin Oleg <kutkino@list.ru>
2023-08-12 16:50:34 +0700

    12.08.23 lens_filling algorithm 1.1

--- utilits/lensys_filling.py
+++ utilits/lensys_filling.py
@@ -3,49 +3,61 @@ import matplotlib.path
 import matplotlib.pyplot as plt
 from scipy import integrate
 
-xlim=[-2,2]
-ylim=[-2,2]
-R=0.3
-Step=500
+
+R=0.05
+Step=1000
+a=0.5
+b=0.25
 
 xmap=[]
 ymap=[]
 xcircle=[]
 ycircle=[]
+xcircle.append(-100)
+ycircle.append(-100)
+xlens=[]
+ylens=[]
+xlens.append(-5)
+ylens.append(-5)
+
+xlim=[-3,3]
+ylim=[-3,3]
 plt.xlim(xlim)
 plt.ylim(ylim)
-plt.grid()
+# plt.grid()
 axes = plt.gca()
 axes.set_aspect("equal")
-xcircle.append(0)
-ycircle.append(0)
-circle = matplotlib.patches.Circle((xlim[1], ylim[1]), radius=R, fill=False)
-axes.add_patch(circle)
+xlim=[-2,2]
+ylim=[-2,2]
+
 
 #переводит номер точки в массиве в координату
 def transp(temp, count):
     return(temp[0] + 2*temp[1] /Step * count)
 
+def integrator(numx,numy):
+    temp = integrate.quad(lambda x: (R**2+(x-xmap[numx])**2)**0.5, xlim[0], xlim[1])
+    return (temp[0])
+
 def test(numx,numy):
-    for i in range(len(xcircle)):
-        if ((xmap[numx]-xcircle[i])**2+(ymap[numy]-ycircle[i])**2)**0.5<2*R:
+    for i in range(len(xlens)):
+        if ((xmap[numx]-xlens[i])**2+(ymap[numy]-ylens[i])**2)**0.5<2*R:
             return (False)
-    xcircle.append(xmap[numx])
-    ycircle.append(ymap[numy])
-    return(True)
+    return (True)
 
-
+def parabola(x):
+    return a*x**2+b
 
 for i in range(Step+1):
     xmap.append(transp(xlim, i))
     for j in range(Step+1):
         ymap.append(transp(ylim, j))
-        test(i,j)
-        # if test(i,j):
-            # circle = matplotlib.patches.Circle((xmap[i], ymap[j]), radius=R, fill=False)
-            # axes.add_patch(circle)
+        if test(i,j) and (xmap[i]<=parabola(ymap[j])) and (xmap[i]>=-parabola(ymap[j])):
+            xlens.append(xmap[i])
+            ylens.append(ymap[j])
 
-for i in range(len(xcircle)):
-    circle = matplotlib.patches.Circle((xcircle[i], ycircle[i]), radius=R, fill=False)
+
+for i in range(len(xlens)):
+    circle = matplotlib.patches.Circle((xlens[i], ylens[i]), radius=R, fill=True)
     axes.add_patch(circle)
 plt.show()
